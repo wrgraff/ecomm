3fec4571bd240459d1314ffc6113ba307829006a
Artur Trifonov <wrgraff@gmail.com>
2020-12-04 04:37:07 +0300

    create server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi there')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hi there!')
+});
+
+app.listen(3000, () => {
+    console.log('listening')
+})
