173218495cd9a24d8896c90cf718362270603e75
Artur Trifonov <wrgraff@gmail.com>
2020-12-04 14:22:16 +0300

    add form data parsing

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hi there!')
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign up</button>
+            </form>
+        </div>
+    `)
+});
+
+app.post('/', (req, res) => {
+    req.on('data', data => {
+        const parsed = data.toString('utf8').split('&');
+        const formData = {};
+
+        for (let pair of parsed) {
+            const [key, value] = pair.split('=');
+            formData[key] = value;
+        }
+
+        console.log(formData);
+    })
+    res.send('Account created');
 });
 
 app.listen(3000, () => {
