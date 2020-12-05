a340cf0d54c169b0ac4bd591576fee50903c5742
Artur Trifonov <wrgraff@gmail.com>
2020-12-05 18:55:19 +0300

    add users repository

---
+++ repositories/users.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+class UsersRepository {
+    constructor(filename) {
+        if (!filename) {
+            throw new Error('Creating a repository requires a filename');
+        }
+
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            fs.writeFileSync(this.filename, '[]');
+        }
+    };
+
+    async getAll() {
+        const contents = await fs.promises.readFile(this.filename, {
+            encoding: 'utf-8'
+        });
+
+        console.log(contents)
+    }
+};
+
+const test = async () => {
+    const repo = new UsersRepository('users.json');
+
+    await repo.getAll();
+}
+
+test();
